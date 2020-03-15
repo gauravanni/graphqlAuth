2b53eb97c358e0737d4351b3f7dd2824f1347abe
gauravanni <gaurav.kr.9637@gmail.com>
2020-03-15 09:57:22 +0530

    port changes

--- src/index.js
+++ src/index.js
@@ -10,10 +10,20 @@ const server = new GraphQLServer({
     resolvers
 })
 
+const options = {
+    port: process.env.PORT || 4000
+  }
+  
+
 mongoose.connect(`mongodb+srv://gaurav:gaurav@cluster0-txbot.mongodb.net/test`)
 .then(()=>{
     console.log('mongodb connected')
-    server.start(() => console.log('Server is running on localhost:4000'))
+    //server.start(() => console.log('Server is running on localhost:4000'))
+    server.start(options, ({ port }) =>
+  console.log(
+    `Server started, listening on port ${port} for incoming requests.`,
+  ),
+)
 }).catch((err)=>{
     console.error(err)
 })
